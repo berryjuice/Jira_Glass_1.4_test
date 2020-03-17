488d096078e0488d68ebc9bbc080c01f13f56016
Adam Landfrasz <adam.landfrasz@gmail.com>
2020-03-17 14:43:34 +0100

    glass page elements added

--- src/main/java/pages/GlassPage.java
+++ src/main/java/pages/GlassPage.java
@@ -14,13 +14,19 @@ import java.util.List;
 public class GlassPage extends Page {
     private By general = By.xpath("//li[@id='glass-general-nav']/a");
     private By issueTypes = By.xpath("//li[@id='glass-workflow-nav']/a");
-    private By people = By.xpath("//li[@id='glass-people-nav']/a");
+    private By improvementIssueType = By.xpath("//*[@data-issue-type='Improvement']/a");
     private By permissions = By.xpath("//li[@id='glass-permissions-nav']/a");
 
     private By componentsTab = By.xpath("//a[@id='aui-uid-1']");
     private By versionsTab = By.xpath("//a[@id='aui-uid-2']");
     private By schemesTab = By.xpath("//a[@id='aui-uid-3']");
 
+    private By basicSummaryLink = By.xpath("//h2[contains(text(), 'Basic Summary')]/a");
+    private By componentsLink = By.xpath("//h2[span[contains(text(), 'Components')]]/a");
+    private By permissionsLink = By.xpath("//h2[span[contains(text(), 'Permission')]]/a");
+    private By workflowLink = By.xpath("//h2[span[contains(text(), 'Workflow')]]/a");
+
+    private By showTransitionLabels = By.xpath("//input[@id='show-transition-labels']");
 
     public GlassPage(WebDriver driver) {
         super(driver);
