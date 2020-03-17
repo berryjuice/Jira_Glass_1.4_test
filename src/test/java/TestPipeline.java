95383241b8f4a7dc5f7c437ffd67d86774613d33
Katalin Nemeth <zeoramail@gmail.com>
2020-03-17 11:00:12 +0100

    pipeline test added

--- src/test/java/TestPipeline.java
+++ src/test/java/TestPipeline.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestPipeline {
+    @Test
+    void test() {
+        int n = 5;
+        Assertions.assertEquals(5, n);
+    }
+
+    @Test
+    void test2() {
+        int m = 2;
+        Assertions.assertEquals(2, m);
+    }
 }
