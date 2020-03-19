d2b783c11a16a3eb283fc1aef1cad4d9d836e4bd
Katalin Nemeth <zeoramail@gmail.com>
2020-03-19 13:33:05 +0100

    BaseTest localized

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,6 +3,7 @@ package tests;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -21,7 +22,7 @@ public abstract class BaseTest {
 
     @BeforeAll
     protected static void setUp() throws MalformedURLException {
-        String fullGridUrl = GRID_URL.replace("{PASSWORD}", GRID_PW);
+        /*String fullGridUrl = GRID_URL.replace("{PASSWORD}", GRID_PW);
         switch (BROWSER) {
             case "chrome":
                 driver = new RemoteWebDriver(new URL(fullGridUrl), new ChromeOptions());
@@ -29,7 +30,8 @@ public abstract class BaseTest {
             case "firefox":
                 driver = new RemoteWebDriver(new URL(fullGridUrl), new FirefoxOptions());
                 break;
-        }
+        }*/
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
